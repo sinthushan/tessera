a9c94b55171ea579703f6a507d4e809a9b7aa41f
sinthushan <sinthushan@gmail.com>
2025-01-18 22:56:07 -0500

    basic model structure

--- backend/officer/models.py
+++ backend/officer/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Team(models.Model):
+    team_name = models.CharField(max_length=50, unique=True)
+    
 
 class Officer(AbstractUser):
-    pass
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE,related_name="team_members" )
+    date_joined = models.DateTimeField()
--- backend/ticket/models.py
+++ backend/ticket/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from officer.models import Officer
+
+class Ticket(models.Model):
+    subject = models.CharField(max_length=250)
+    description = models.TextField(max_length=500)
+    created_by = models.ForeignKey(Officer, on_delete=models.CASCADE, related_name="created_tickets")
+    assigned_to = models.ForeignKey(Officer,blank = True, null = True, on_delete=models.SET_NULL, related_name="assigned_tickets")
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField()
